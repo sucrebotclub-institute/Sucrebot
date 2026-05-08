16fb7a1551d31a33ca38fd1f4c3cfe078fbedcfc
sucrebotclub-institute <sucrebotclub@tecnologicosucre.edu.ec>
2026-05-08 08:14:13 -0500

    Update config.js

--- shared/js/config.js
+++ shared/js/config.js
@@ -1,1 +1,7 @@
-
+// config.js - Configuración centralizada de SucreBot
+const CONFIG = {
+  DEPLOYMENT_ID: 'AKfycbydhor7cdkit0KCMfvMO7AdtUw5p-sqSBwxKwF7aR61v0PcJLwMWb7DqAQJ5ou4rYXr',
+  GAS_URL: function() {
+    return `https://script.google.com/macros/s/${this.DEPLOYMENT_ID}/exec`;
+  }
+};
