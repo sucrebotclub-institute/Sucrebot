8629f6ce8456fd26b01ab15d84bb69cb7d1c60e6
sucrebotclub-institute <sucrebotclub@tecnologicosucre.edu.ec>
2026-05-09 16:32:31 -0500

    Update config.js

--- shared/js/config.js
+++ shared/js/config.js
@@ -1,7 +1,20 @@
 // config.js - Configuración centralizada de SucreBot
+
 const CONFIG = {
-  DEPLOYMENT_ID: 'AKfycbydhor7cdkit0KCMfvMO7AdtUw5p-sqSBwxKwF7aR61v0PcJLwMWb7DqAQJ5ou4rYXr',
+  // ID de deployment actual del Google Apps Script
+  DEPLOYMENT_ID: 'AKfycbxc-nLopIe6C5Rh0O22oEwCwgYZfk68PTl2YzZTZarNdmazdL0z2g7jm-XUjhH_oIGkzw',
+  
+  // Función para obtener la URL completa del GAS
   GAS_URL: function() {
     return `https://script.google.com/macros/s/${this.DEPLOYMENT_ID}/exec`;
-  }
+  },
+  
+  // OAuth Client ID de Google
+  OAUTH_CLIENT_ID: '14154960360-fofn56epv2rsiq882sni5ku0q1idemg4.apps.googleusercontent.com',
+  
+  // Emails del staff autorizado
+  STAFF_EMAILS: [
+    'ftipantocta@tecnologicosucre.edu.ec',
+    'sucrebotclub@tecnologicosucre.edu.ec'
+  ]
 };
