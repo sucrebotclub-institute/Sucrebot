61b1a33f03590e8451022464cd815835bb25a756
sucrebotclub-institute <sucrebotclub@tecnologicosucre.edu.ec>
2026-05-23 11:38:37 -0500

    Update config.js

--- shared/js/config.js
+++ shared/js/config.js
@@ -2,7 +2,7 @@
 
 const CONFIG = {
   // ID de deployment actual del Google Apps Script
-  DEPLOYMENT_ID: 'AKfycbw3vNIzaLk0l6h69lpT5AU84U1DgbdljITwdO3vyR7mlU0DBjOVdNxv6XccpwTcrlifFA',
+  DEPLOYMENT_ID: 'AKfycbzOM6cV6lxjZF3A6uSHRZ_cViZfsBHbV0vqMTJivhmz6tFxOVfNveRaDBRXlZM1zjLkNQ',
   
   // Función para obtener la URL completa del GAS
   GAS_URL: function() {
