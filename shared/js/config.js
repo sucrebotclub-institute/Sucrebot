d98e1e357a00aad1f0e16cbb8be9bb9a71063d92
sucrebotclub-institute <sucrebotclub@tecnologicosucre.edu.ec>
2026-06-22 18:48:26 -0500

    Update config.js

--- shared/js/config.js
+++ shared/js/config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
 
   // ── Google Apps Script ──────────────────────────────────────
-  DEPLOYMENT_ID: 'AKfycbxx0hTbdKawkTnMQx8rbYvT7TMxg3KVGnQlaxw7WnP-7trc3Joo2xHf52-1xvuTNbRppA',   
+  DEPLOYMENT_ID: 'AKfycbxx0hTbdKawkTnMQx8rbYvT7TMxg3KVGnQlaxw7WnP-7trc3Joo2xHf52-1xvuTNbRppA',    
 
   GAS_URL: function() {
     return 'https://script.google.com/macros/s/' + this.DEPLOYMENT_ID + '/exec';
